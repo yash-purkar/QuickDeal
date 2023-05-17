37b625113571efcfaf67cfab5740d64ccb3b1792
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-17 18:03:57 +0530

    toggle search bar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,30 +5,41 @@ import { RxCross1 } from 'react-icons/rx'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 
-const getActiveStyle = ({ isActive }) => ({ borderBottom: "1px solid var(--primary)" })
 
 export const Navbar = () => {
-  const [menuClass, setMenuClass] = useState("hide-menu")
+  const [menuClass, setMenuClass] = useState("hide-menu");
+  const [searchBar, setSearchBar] = useState(false);
 
   const handleMenuClick = (data) => {
     const updatedDisplay = data === "hide" ? "hide-menu" : "menus";
     setMenuClass(updatedDisplay)
   }
 
+  const handleToggleSearhBar = () => { }
+
   return (
     <>
-      <nav className='navigation'>
+      <nav className='navigation flex justify-between align-center'>
         <h1 className='navigation-header'><NavLink className="header-link" to="/" >QuickDeal</NavLink></h1>
+
+
+        {
+          searchBar && <div>
+            <input type="text" className='search-bar' placeholder='Search Product' />
+            <span></span>
+          </div>
+        }
+
 
         {
           menuClass === "hide-menu" ? <div className='navigation-menu' onClick={() => handleMenuClick()}><AiOutlineMenu /></div> : <div className='navigation-menu' onClick={() => handleMenuClick("hide")}><RxCross1 /></div>
         }
 
         <ul className={menuClass}>
-          <li className='menu-item'><NavLink className="nav-link"><AiOutlineSearch /></NavLink></li>
+          <li className='menu-item'><NavLink onClick={() => setSearchBar(prev => !prev)} className="nav-link">{searchBar ? <RxCross1 /> : <AiOutlineSearch />}</NavLink></li>
 
           <li className='menu-item'>
-            <NavLink className="nav-link" to="/productlisting" ><MdOutlineLocalMall /></NavLink>
+            <NavLink className="nav-link" to="/productlisting"><MdOutlineLocalMall /></NavLink>
           </li>
           <li className='menu-item'><NavLink to="/cart" className="nav-link"><AiOutlineShoppingCart /></NavLink></li>
           <li className='menu-item'><NavLink to="/wishlist" className="nav-link"><AiOutlineHeart /></NavLink></li>
@@ -36,7 +47,7 @@ export const Navbar = () => {
         </ul>
 
         <ul className='menus-md' >
-          <li className='menu-item'><NavLink className="nav-link"><AiOutlineSearch /></NavLink></li>
+          <li className='menu-item'><NavLink onClick={() => setSearchBar(prev => !prev)} className="nav-link">{searchBar ? <RxCross1 /> : <AiOutlineSearch />}</NavLink></li>
           <li className='menu-item'><NavLink className="nav-link" to="/productlisting" ><MdOutlineLocalMall /></NavLink></li>
           <li className='menu-item'><NavLink to="/cart" className="nav-link"><AiOutlineShoppingCart /></NavLink></li>
           <li className='menu-item'><NavLink to="/wishlist" className="nav-link"><AiOutlineHeart /></NavLink></li>
