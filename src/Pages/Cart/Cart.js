4614ea31f68b66a90719884b4dd534be17840a90
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-21 18:12:14 +0530

    docs: styled coupon box

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { ImCross } from 'react-icons/im'
 import './Cart.css'
 import { SingleCartProduct } from './Components/SingleCartProduct/SingleCartProduct'
 import { PriceDetails } from './Components/CartPriceDetails/PriceDetails'
@@ -24,6 +25,30 @@ export const Cart = () => {
 
         </div>
         <PriceDetails cartData={cart} />
+        {/* ** */}
+        <div className='main-coupon-container flex justify-center align-center' >
+
+
+
+          <div className='coupon-container flex direction-column justify-between '>
+            <div className='coupon-box-head flex justify-between margin-bottom-1'>
+              <h3>Apply Coupon</h3>
+              <span className='cross-icon cursor-pointer'><ImCross /></span>
+            </div>
+            <label htmlFor="coupon50" className='discount-type font-md flex align-center font-sm'>
+              <input type="radio" name="coupon" className='discount-radio' />
+              50% OFF:DIWALI_DHAMAKA
+            </label>
+            <label htmlFor="coupon10" className='discount-type font-md flex align-center margin-bottom-1 font-sm'>
+              <input type="radio" name="coupon" className='discount-radio' />
+              10% OFF:NEW_USER
+            </label>
+
+            <button className='apply-btn cursor-pointer'>Apply</button>
+          </div>
+
+        </div>
+        {/* ** */}
       </div>
     </>
   )
