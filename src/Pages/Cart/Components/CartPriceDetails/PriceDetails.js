844295820b7739f9a00d568baf38091a82d2b43b
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-22 21:33:48 +0530

    refactor: update code

--- src/Pages/Cart/Components/CartPriceDetails/PriceDetails.js
+++ src/Pages/Cart/Components/CartPriceDetails/PriceDetails.js
@@ -6,12 +6,13 @@ import { CouponBox } from '../CouponBox/CouponBox'
 
 import './PriceDetails.css'
 import { OrderState } from '../../../../Contexts/Order/OrderContext'
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export const PriceDetails = ({ cartData, }) => {
   const [isHideCouponBox, setIsHideCouponBox] = useState(true);
+  const { orderDispatch } = OrderState()
 
-
+  const navigate = useNavigate();
 
   const { couponInfo, setCouponInfo } = OrderState();
 
@@ -19,7 +20,7 @@ export const PriceDetails = ({ cartData, }) => {
   const newPrice = cartData?.reduce((acc, curr) => curr.newPrice * curr.qty + acc, 0);
   const oldPrice = cartData?.reduce((acc, curr) => curr.oldPrice * curr.qty + acc, 0);
 
-  const couponDisccount = (newPrice * couponInfo.value) / 100;
+  const couponDiscount = ((newPrice * couponInfo.value) / 100);
 
 
   const clearCoupon = () => {
@@ -27,7 +28,18 @@ export const PriceDetails = ({ cartData, }) => {
   }
 
 
+  const handleCheckOut = () => {
+    navigate("/checkout");
 
+    orderDispatch({
+      type: "CHECKOUT", payload: {
+        discount: oldPrice - newPrice,
+        couponDiscount: couponDiscount.toFixed(),
+        totalAmount: (newPrice - couponDiscount).toFixed(),
+        price: oldPrice
+      }
+    })
+  }
 
   return (
     <div className='price-detail-card'>
@@ -47,7 +59,7 @@ export const PriceDetails = ({ cartData, }) => {
 
         <div className='flex justify-between'>
           <p className='sm-fontsize sm-margin-bottom'>Cuopon Discount</p>
-          <p className='sm-fontsize sm-margin-bottom'>-₹ {couponDisccount.toFixed()}</p>
+          <p className='sm-fontsize sm-margin-bottom'>-₹ {couponDiscount.toFixed()}</p>
         </div>
         {
 
@@ -64,7 +76,7 @@ export const PriceDetails = ({ cartData, }) => {
         }
         <div className='flex justify-between total-amt top-margin border-top-1 top-padding-08'>
           <h5 className='sm-fontsize sm-margin-bottom '>Total Amount</h5>
-          <h5 className='sm-fontsize sm-margin-bottom'>₹ {(newPrice - couponDisccount).toFixed()}</h5>
+          <h5 className='sm-fontsize sm-margin-bottom'>₹ {(newPrice - couponDiscount).toFixed()}</h5>
         </div>
 
         <div className='flex justify-between coupon-box '>
@@ -74,11 +86,11 @@ export const PriceDetails = ({ cartData, }) => {
 
       </div>
 
-      <p className='sm-fontsize sm-margin-bottom saved-price-info'>You will save ₹ {(couponDisccount + oldPrice - newPrice).toFixed()} on this order</p>
+      <p className='sm-fontsize sm-margin-bottom saved-price-info'>You will save ₹ {(couponDiscount + oldPrice - newPrice).toFixed()} on this order</p>
 
-      <NavLink to="/checkout" >
-        <button className='checkout-btn width-100 cursor-pointer'>Checkout</button>
-      </NavLink>
+
+      <button className='checkout-btn  cursor-pointer' onClick={handleCheckOut}>Checkout</button>
+
 
 
 
--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Checkout.css'
+import { OrderState } from '../../Contexts/Order/OrderContext';
+import { DataState } from '../../Contexts/Data/DataContext';
 
 export const Checkout = () => {
-  const amount = 6000;
+  const { state: { cart } } = DataState();
+  const { orderState: { price, discount, couponDiscount, totalAmount } } = OrderState();
+
+
 
 
   const handlePlaceOrder = () => {
     var options = {
       key: "rzp_test_eIfzqLCuOyYMmK",
       key_secret: "key_secret",
-      amount: amount * 100,
+      amount: totalAmount * 100,
       currency: "INR",
       name: "QuickDeal",
       description: "For testing purpose",
@@ -34,60 +39,58 @@ export const Checkout = () => {
   }
   return (
     <div className='checkout-container flex direction-column'>
-      <h3 className='text-uppercase border-top-1  padding-1 font-1 padding-left-0 text-center bottom-border-1'>order details</h3>
+      <h3 className='text-uppercase border-top-1  padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>order details</h3>
       <div className='flex justify-between'>
         <p className='font-bold top-padding-08  bottom-margin-md'>Item</p>
-        <p className='font-bold top-padding-08  bottom-margin-md'>Qty</p>
+        <p className='font-bold top-padding-08  bottom-margin-md '>Qty</p>
       </div>
 
       {/*  */}
       <div className='padding-bottom-1'>
 
-        <div className='flex justify-between '>
-          <p>Roadster</p>
-          <p>1</p>
-        </div>
-        <div className='flex justify-between'>
-          <p>Puma</p>
-          <p>5</p>
-        </div>
+        {
+          cart.map(prod => <div key={prod._id} className="flex justify-between">
+            <p>{prod.itemName}</p>
+            <p>{prod.qty}</p>
+          </div>)
+        }
 
       </div>
       {/*  */}
-      <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1'>price details</h3>
+      <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>price details</h3>
 
       {/* */}
       <div className='padding-bottom-1 '>
 
         <div className='flex justify-between'>
-          <p>Price(3) </p>
-          <p>₹ 2399</p>
+          <p>Price({cart.length}) </p>
+          <p>₹ {price}</p>
         </div>
 
         <div className='flex justify-between'>
           <p>Discount</p>
-          <p>-₹ 1230</p>
+          <p>-₹ {discount}</p>
         </div>
 
         <div className='flex justify-between'>
           <p>Delievery Charges</p>
-          <p className='text-uppercse'>free</p>
+          <p className='text-uppercase'>free</p>
         </div>
 
         <div className='flex justify-between'>
           <p>Coupon Discount</p>
-          <p>₹ 0</p>
+          <p>-₹ {couponDiscount}</p>
         </div>
 
-        <div className='flex justify-between'>
+        <div className='flex justify-between top-margin font-bold'>
           <p>Total Amount</p>
-          <p>₹ 1169.00</p>
+          <p>₹ {totalAmount}</p>
         </div>
       </div>
 
       {/*  */}
 
-      <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1'>deliver to</h3>
+      <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>deliver to</h3>
 
       <div className='delivery-address margin-bottom-1 font-sm'>
         <p className='font-1 font-bold'>John Doe</p>
