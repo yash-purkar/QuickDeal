6e193a2e60d0204e0ab5fbec9c1c53cde7a19612
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-27 13:03:33 +0530

    refactor: update productDetails

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { AiOutlineStar, AiFillHeart, AiOutlineShoppingCart } from 'react-icons/ai'
 
@@ -8,19 +8,22 @@ import { DataState } from '../../Contexts/Data/DataContext'
 import { addToCart } from '../../Services/Cart/CartServices'
 import { addToWishlist, removeFromWishlist } from '../../Services/Wishlist/WishlistServices'
 import { loginTocontinue } from '../../Services/Toasts/ToastServices'
+import { getProductDetails } from '../../Services/Product/ProductDetailServices'
 
 export const ProductDetail = () => {
   const { productId } = useParams();
+  const [product, setProduct] = useState();
 
-
-  const { state: { products, cart, wishlist, token }, dispatch } = DataState();
+  const { state: { cart, wishlist, token }, dispatch } = DataState();
   const navigate = useNavigate();
   const location = useLocation();
 
   const [disabledBtn, setDisabledBtn] = useState();
 
 
-  const product = products?.find(product => product._id === productId);
+  useEffect(() => {
+    getProductDetails(productId, setProduct);
+  }, [])
 
   const handleAddToCart = (product, dispatch, token, navigate, location) => {
     if (token) {
@@ -54,63 +57,65 @@ export const ProductDetail = () => {
     removeFromWishlist(_id, dispatch, token);
   }
 
-  const { _id, image, rating, reviews, size, category, description, itemName, oldPrice, newPrice, discount, isTrending, inStock, delivery_time, fewLeft } = product
+
   return (
-    <div className='product-detail-container flex justify-center align-center wrap'>
-      <div className='detail-img-box'>
-        <img src={image} alt={itemName} className='detail-img' />
-        {isTrending && <span className='trending'>Trending</span>}
-        {
-          wishlist?.some(product => product._id === _id) ? <span className='like  wishlist-red' onClick={() => handleRemoveWishlist(product._id, dispatch, token)}><AiFillHeart /></span> : <button className='like' disabled={disabledBtn} onClick={() => handleAddToWishlist(product, dispatch, token, navigate, location)} ><AiFillHeart /></button>
-        }
-      </div>
-
-
-      <div className='product-details flex direction-column justify-between'>
-
-        <div className='flex justify-between align-center'>
-
-          <h2 className='font-1-3 header-md'>{itemName}</h2>
-          <div className='detail-star-rating rating-star ' style={{ position: "unset" }}>
-            <span><AiOutlineStar /></span>
-            <span>{rating}</span>
-          </div>
-        </div>
-        <div>
-          <span className='new-price sm-fontsize'>₹{newPrice}</span>
-          <span className='old-price right-margin sm-fontsize'>₹{oldPrice}</span>
-          <span className='discount sm-fontsize'>{discount}% OFF</span>
-        </div>
-        {fewLeft && <p className='few-left font-extra-sm font-bold-md'> Hurry, Only Few Left!</p>}
-        <div>
-          <span className='right-margin font-bold font-sm font-md'>Availability : </span> <span className='in-stock font-md sm-fontsize'>{inStock ? "In Stock" : <span className='font-sm font-md' style={{ color: "red" }}>Out Of Stock</span>}</span>
-        </div>
-        <div>
-          <span className='right-margin font-sm font-bold font-md'>Description : </span>
-          <span className='sm-fontsize line-height'>{description}</span>
-        </div>
-        <div>
-          <span className='right-margin font-bold font-sm font-md'>Size :</span>
-          <span className='sm-fontsize'>{size}</span>
-        </div>
-        <div>
-          <span className='right-margin font-bold font-sm font-md'>Delivery : </span>
-          <span className='sm-fontsize'>{delivery_time}</span>
-        </div>
-
-        <div>
+    <>
+      {product && <div className='product-detail-container flex justify-center align-center wrap'>
+        <div className='detail-img-box'>
+          <img src={product.image} alt={product.itemName} className='detail-img' />
+          {product.isTrending && <span className='trending'>Trending</span>}
           {
-            cart?.some(product => product._id === _id) ? <NavLink to="/cart">
-              <button className="go-to-cart">Go To Cart</button></NavLink> :
-
-              <button className={`${inStock ? "add-to-cart" : "out-of-stock-btn"}`} disabled={!inStock || disabledBtn} onClick={() => handleAddToCart(product, dispatch, token, navigate, location)}>
-
-                {inStock ? "Add To Cart" : <span className='out-of-stock'>OUT OF STOCK</span>}
-              </button>
+            wishlist?.some(prod => prod._id === product._id) ? <span className='like  wishlist-red' onClick={() => handleRemoveWishlist(product._id, dispatch, token)}><AiFillHeart /></span> : <button className='like' disabled={disabledBtn} onClick={() => handleAddToWishlist(product, dispatch, token, navigate, location)} ><AiFillHeart /></button>
           }
         </div>
-      </div>
 
-    </div>
+
+        <div className='product-details flex direction-column justify-between'>
+
+          <div className='flex justify-between align-center'>
+
+            <h2 className='font-1-3 header-md'>{product.itemName}</h2>
+            <div className='detail-star-rating rating-star ' style={{ position: "unset" }}>
+              <span><AiOutlineStar /></span>
+              <span>{product.rating}</span>
+            </div>
+          </div>
+          <div>
+            <span className='new-price sm-fontsize'>₹{product.newPrice}</span>
+            <span className='old-price right-margin sm-fontsize'>₹{product.oldPrice}</span>
+            <span className='discount sm-fontsize'>{product.discount}% OFF</span>
+          </div>
+          {product.fewLeft && <p className='few-left font-extra-sm font-bold-md'> Hurry, Only Few Left!</p>}
+          <div>
+            <span className='right-margin font-bold font-sm font-md'>Availability : </span> <span className='in-stock font-md sm-fontsize'>{product.inStock ? "In Stock" : <span className='font-sm font-md' style={{ color: "red" }}>Out Of Stock</span>}</span>
+          </div>
+          <div>
+            <span className='right-margin font-sm font-bold font-md'>Description : </span>
+            <span className='sm-fontsize line-height'>{product.description}</span>
+          </div>
+          <div>
+            <span className='right-margin font-bold font-sm font-md'>Size :</span>
+            <span className='sm-fontsize'>{product.size}</span>
+          </div>
+          <div>
+            <span className='right-margin font-bold font-sm font-md'>Delivery : </span>
+            <span className='sm-fontsize'>{product.delivery_time}</span>
+          </div>
+
+          <div>
+            {
+              cart?.some(prod => prod._id === product._id) ? <NavLink to="/cart">
+                <button className="go-to-cart">Go To Cart</button></NavLink> :
+
+                <button className={`${product.inStock ? "add-to-cart" : "out-of-stock-btn"}`} disabled={!product.inStock || disabledBtn} onClick={() => handleAddToCart(product, dispatch, token, navigate, location)}>
+
+                  {product.inStock ? "Add To Cart" : <span className='out-of-stock'>OUT OF STOCK</span>}
+                </button>
+            }
+          </div>
+        </div>
+
+      </div>}
+    </>
   )
 }
