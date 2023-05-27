ccca699bb4fb6ec4045ab76cc40c8564e3e72017
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-27 11:54:43 +0530

    style: update css

--- src/Pages/Profile/Components/Address.js
+++ src/Pages/Profile/Components/Address.js
@@ -19,7 +19,7 @@ export const Address = () => {
   return (
     <div className='flex justify-center  '>
       <div className='width-100'>
-        <div className='flex justify-between bottom-border-1 margin-bottom-1'>
+        <div className='flex justify-between align-center bottom-border-1 margin-bottom-1'>
           <h2 className='profile-head my-addr margin-bottom-1'>My Addresses</h2>
           <h4 className='margin-bottom-1 cursor-pointer add-new-address font-1-2' onClick={() => setIsHideForm(false)}>Add New Address+</h4>
         </div>
@@ -29,9 +29,9 @@ export const Address = () => {
             } = addr;
             return (
               <div className='margin-bottom-1 bottom-border-1' key={i}>
-                <p className='profile-label address-user-name letter-spacing font-1-2'>{name}</p>
+                <p className='profile-label address-user-name letter-spacing  singleAddr'>{name}</p>
 
-                <div className='font-1 letter-spacing flex direction-column'>
+                <div className='letter-spacing flex direction-column addr-detail'>
                   <span className='capitalize'>{street}</span>
                   <span className='capitalize'>{cityName},{state} . {postalCode}</span>
                   <span className='capitalize'>{country}</span>
