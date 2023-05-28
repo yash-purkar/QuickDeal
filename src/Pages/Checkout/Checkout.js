93d8e54d0a6ed90a4fdfb5d71eee773628a17b0f
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-05-28 23:43:35 +0530

    docs: update text

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Checkout.css'
 import { OrderState } from '../../Contexts/Order/OrderContext';
 import { DataState } from '../../Contexts/Data/DataContext';
@@ -74,104 +74,115 @@ export const Checkout = () => {
     });
 
   }
+
+  useEffect(() => {
+    if (cart.length === 0) {
+      navigate("/productlisting")
+    }
+  }, [])
   return (
     <>
       {
-        isOrderPlaced ? <h1 className='text-center top-margin-5'>Your Ordered Placed</h1> : <div className='checkout-main-container flex justify-around  wrap'>
-          {/* addr */}
-          <div className='checkout-addresses '>
-            <>
-              {
-                addresses.length > 0 ?
-                  <>{addresses?.map((addr) => {
-                    const { id, name, street, cityName, state, country, postalCode, mobileNumber } = addr
-                    return (
-                      <div key={id} className='single-checkout-address cursor-pointer' onClick={() => setSelectedAddress(addr)}>
-                        <label htmlFor="" className='checkout-addr-name'>
-                          <input type="radio" name='address' checked={selectedAddress.id === id} />
-                          <span className='left-padding-sm'>{name}</span>
-                        </label>
-                        <div className='left-padding-sm'>
-                          <p>{street}, {cityName}, {state}, {postalCode}</p>
-                          <p>{country}</p>
-                          <p>Mobile Number: {mobileNumber}</p>
-                        </div>
-                      </div>
-                    )
-                  })}</>
-                  : <h1 className='select-address-heading letter-spacing'>Add The Address First</h1>}
-
-            </>
-          </div>
-          {/*  */}
-
-          <div className='checkout-container flex direction-column justify-between'>
-            <h3 className='text-uppercase border-top-1  padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>order details</h3>
-            <div className='flex justify-between'>
-              <p className='font-bold top-padding-08  bottom-margin-md'>Item</p>
-              <p className='font-bold top-padding-08  bottom-margin-md '>Qty</p>
-            </div>
-
-            {/*  */}
-            <div className='padding-bottom-1'>
-
-              {
-                cart.map(prod => <div key={prod._id} className="flex justify-between">
-                  <p>{prod.itemName}</p>
-                  <p>{prod.qty}</p>
-                </div>)
-              }
-
-            </div>
-            {/*  */}
-            <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>price details</h3>
-
-            {/* */}
-            <div className='padding-bottom-1 '>
-
-              <div className='flex justify-between'>
-                <p>Price({cart.length}) </p>
-                <p>₹ {price}</p>
-              </div>
-
-              <div className='flex justify-between'>
-                <p>Discount</p>
-                <p>-₹ {discount}</p>
-              </div>
-
-              <div className='flex justify-between'>
-                <p>Delievery Charges</p>
-                <p className='text-uppercase'>free</p>
-              </div>
-
-              <div className='flex justify-between'>
-                <p>Coupon Discount</p>
-                <p>-₹ {couponDiscount}</p>
-              </div>
-
-              <div className='flex justify-between top-margin font-bold checkout-total letter-spacing'>
-                <p>Total Amount</p>
-                <p>₹ {totalAmount}</p>
-              </div>
-            </div>
-
-            {/*  */}
-
-            <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>deliver to</h3>
-
-            {addresses?.length > 0 && <div className='delivery-address margin-bottom-1 font-sm'>
-              <p className='font-1 address-name font-bold'>{name}</p>
-              <p>{street}, {cityName}, {state}, - {postalCode}</p>
-              <p>{country}</p>
-              <p>Mobile Number :{mobileNumber}</p>
-            </div>}
-
-
-
-            <button className='place-order-btn cursor-pointer' onClick={handlePlaceOrder}>Place Order</button>
-
-          </div>
+        isOrderPlaced ? <div>
+          <h1 className='top-margin-5 text-center letter-spacing margin-bottom-1 order-succesfully'>Order Placed Successfully</h1>
+          <p className='text-center font-bold'>Thank you for chossing Us</p>
         </div>
+          :
+          <div className='checkout-main-container flex justify-around  wrap'>
+            {/* addr */}
+            <div className='checkout-addresses '>
+              <>
+                {
+                  addresses.length > 0 ?
+                    <>{addresses?.map((addr) => {
+                      const { id, name, street, cityName, state, country, postalCode, mobileNumber } = addr
+                      return (
+                        <div key={id} className='single-checkout-address cursor-pointer' onClick={() => setSelectedAddress(addr)}>
+                          <label htmlFor="" className='checkout-addr-name'>
+                            <input type="radio" name='address' checked={selectedAddress.id === id} />
+                            <span className='left-padding-sm'>{name}</span>
+                          </label>
+                          <div className='left-padding-sm'>
+                            <p>{street}, {cityName}, {state}, {postalCode}</p>
+                            <p>{country}</p>
+                            <p>Mobile Number: {mobileNumber}</p>
+                          </div>
+                        </div>
+                      )
+                    })}</>
+                    : <h1 className='select-address-heading letter-spacing'>Add The Address First</h1>}
+
+              </>
+            </div>
+            {/*  */}
+
+            <div className='checkout-container flex direction-column justify-between'>
+              <h3 className='text-uppercase border-top-1  padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>order details</h3>
+              <div className='flex justify-between'>
+                <p className='font-bold top-padding-08  bottom-margin-md'>Item</p>
+                <p className='font-bold top-padding-08  bottom-margin-md '>Qty</p>
+              </div>
+
+              {/*  */}
+              <div className='padding-bottom-1'>
+
+                {
+                  cart.map(prod => <div key={prod._id} className="flex justify-between">
+                    <p>{prod.itemName}</p>
+                    <p>{prod.qty}</p>
+                  </div>)
+                }
+
+              </div>
+              {/*  */}
+              <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>price details</h3>
+
+              {/* */}
+              <div className='padding-bottom-1 '>
+
+                <div className='flex justify-between'>
+                  <p>Price({cart.length}) </p>
+                  <p>₹ {price}</p>
+                </div>
+
+                <div className='flex justify-between'>
+                  <p>Discount</p>
+                  <p>-₹ {discount}</p>
+                </div>
+
+                <div className='flex justify-between'>
+                  <p>Delievery Charges</p>
+                  <p className='text-uppercase'>free</p>
+                </div>
+
+                <div className='flex justify-between'>
+                  <p>Coupon Discount</p>
+                  <p>-₹ {couponDiscount}</p>
+                </div>
+
+                <div className='flex justify-between top-margin font-bold checkout-total letter-spacing'>
+                  <p>Total Amount</p>
+                  <p>₹ {totalAmount}</p>
+                </div>
+              </div>
+
+              {/*  */}
+
+              <h3 className='text-uppercase border-top-1  margin-bottom-1 padding-1 font-1 padding-left-0 text-center bottom-border-1 color-primary letter-spacing'>deliver to</h3>
+
+              {addresses?.length > 0 && <div className='delivery-address margin-bottom-1 font-sm'>
+                <p className='font-1 address-name font-bold'>{name}</p>
+                <p>{street}, {cityName}, {state}, - {postalCode}</p>
+                <p>{country}</p>
+                <p>Mobile Number :{mobileNumber}</p>
+              </div>}
+
+
+
+              <button className='place-order-btn cursor-pointer' onClick={handlePlaceOrder}>Place Order</button>
+
+            </div>
+          </div>
       }
     </>
   )
